ecf71f6a9900c2a4b2059edd5f080561e14f1fa5
Yann Richet <yann.richet@laposte.net>
2022-05-20 19:03:54 +0200

    fixes

--- map.R
+++ map.R
@@ -8,12 +8,12 @@ xy2lonlat = function(xy1,lonlat1,xy2,lonlat2) {
   })
 }
 
-#' @test map_geo(Geo_Loire_V6.slf, xy1=c(664649.1875,	6736072.5),
+#' @test map_geo("Geo_Loire_V6.slf", xy1=c(664649.1875,	6736072.5),
 #'                                 lonlat1=c(2.528116,47.723969),
 #'                                 xy2=c(635994.0,	6750523.0),
 #'                                 lonlat2=c(2.144066829311161,47.85201791739933),
-#'                                 raster=raster::getData(name="GADM", country="FRA", level=0))
-map_geo = function(geo.slf, xy1, lonlat1,xy2,lonlat2, raster=NULL, title=NULL) {
+#'                                 raster=raster::getData(name="GADM", country="FRA", level=5))
+map_geo = function(geo.slf, xy1, lonlat1,xy2,lonlat2, raster=NULL, title=NULL,xlab="lon",ylab="lat",...) {
   geo = telemac::read_geo(geo.slf)
 
   col.fun = colorRamp(c("blue", "green", "red"))
@@ -24,11 +24,11 @@ map_geo = function(geo.slf, xy1, lonlat1,xy2,lonlat2, raster=NULL, title=NULL) {
   geo_lonlat = lonlat(cbind(geo$x,geo$y))
 
   if (is.null(raster)) {
-    plot(apply(min,1,geo_lonlat),apply(max,1,geo_lonlat),type='n',main=title)
+    plot(apply(geo_lonlat,1,min),apply(geo_lonlat,1,max),type='n',main=title, xlab=xlab,ylab=ylab,...)
   } else {
-    if (typeof(raster)!="raster") 
-      stop("raster should come from raster::getData")
-    plot(raster, main=title)
+    #if (typeof(raster)!="raster") 
+    #  stop("raster should come from raster::getData")
+    plot(raster, xlim=c(min(geo_lonlat[,1]),max(geo_lonlat[,1])),ylim=c(min(geo_lonlat[,2]),max(geo_lonlat[,2])), main=title, xlab=xlab, ylab=yalb, ...)
   }
 
   points(geo_lonlat[,1],geo_lonlat[,2], col = rgb(cols[,1],cols[,2],cols[,3],0.1),pch='.')
